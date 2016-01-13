5064325e29fa2f1906667e8617d6ed7b56ffb025
vdulevich <vdulevich@gloriumtech.com>
2016-01-13 19:51:55 +0300

    1. Lesson7

--- Part2/routes/users.js
+++ Part2/routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
+var ObjectID = require('mongodb').ObjectID;
+var User = require('../models/User').User;
 var router = express.Router();
+var HttpError = require('../errors/index').HttpError;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find({}, function(err, users){
+    if(err) next(err);
+    console.log('Users found')
+    res.json(users);
+  });
+});
+
+/* GET users listing. */
+router.get('/:id', function(req, res, next) {
+  try {
+    var userId = new ObjectID(req.params.id);
+  } catch (e) {
+    return next(new HttpError(404, 'User not found'));
+  }
+  User.findById(userId, function(err, user){
+    if(err) next(err);
+    if(!user) {
+      return next(new HttpError(404, 'User not found'));
+    }
+    res.json(user);
+  });
 });
 
 module.exports = router;
