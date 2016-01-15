f538b0190d213797ba21df9f19663a62e7684443
vdulevich <vdulevich@gloriumtech.com>
2016-01-15 18:00:28 +0300

    1. Lesson9

--- Part2/middleware/sendHttpError.js
+++ Part2/middleware/sendHttpError.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res, next){
     res.sendHttpError = function(err) {
         res.status(err.status);
-        if(res.req.headers['x-requested-with'] == 'XMLHtttpRequest'){
+        if(res.req.headers['x-requested-with'] == 'XMLHttpRequest'){
             res.json(err);
         } else {
             res.render('error', { error: err });
